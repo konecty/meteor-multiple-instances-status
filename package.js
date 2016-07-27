1eb7a91f23b00b24a8bd1c88011aa394ba9437da
Diego Sampaio <chinello@gmail.com>
2016-07-27 10:22:49 -0300

    Update nooitaf:colors version

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'konecty:multiple-instances-status',
   summary: 'Keep a collection with active servers/instances',
-  version: '1.0.6',
+  version: '1.0.6_1',
   git: 'https://github.com/Konecty/meteor-multiple-instances-status'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
 
-  api.use('nooitaf:colors@0.0.2');
+  api.use('nooitaf:colors@1.1.2_1');
 
   api.addFiles('multiple-instances-status.js', ['server']);
 
